a85152f8e1cb6bea9a4bbbf67733377c9c8aeb15
Jellopy <parnupat.j@gmail.com>
2020-12-22 19:40:53 +0700

    init json

--- routes/mock.js
+++ routes/mock.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router();
 var novaSales = require('../db/nova_sales.json')
+var novaValue = require('../db/nova_value.json')
 router.get('/nova_dynamic-form',(req,res)=> {
   res.status(200).json(novaSales)
 })
-
+router.get('/nova/getValue',(req,res)=> {
+  res.status(200).json(novaValue)
+})
 module.exports = router
